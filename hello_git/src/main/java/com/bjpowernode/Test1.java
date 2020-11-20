194c3cc1347752ea0e55e3e1bd89f97fd1d9c077
guojiahui <1209681667@qq.com>
2020-11-20 23:25:00 +0800

    test_ignore2

--- hello_git/src/main/java/com/bjpowernode/Test1.java
+++ hello_git/src/main/java/com/bjpowernode/Test1.java
@@ -3,5 +3,6 @@ package com.bjpowernode;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("1111111111");
     }
 }
